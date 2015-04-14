e7c6c66bd2fab4b07b79fcbef472b83e728a4b27
Gabriel <gabrielg.desouza@gmail.com>
2015-04-14 20:30:28 +0000

    Set up the finals state of the server.

--- Ranking/Ranking/Controllers/RankController.cs
+++ Ranking/Ranking/Controllers/RankController.cs
@@ -14,14 +14,22 @@ namespace Ranking.Controllers
         // GET: Rank
         public ActionResult Index()
         {
-            
-            return View("RankView",_rankContext.Ranks.ToList());
+
+            return View("RankView", _rankContext.Ranks.OrderByDescending(x => x.Score).ToList());
         }
 
         [HttpGet]
         public ActionResult Rank()
         {
-            return Json(_rankContext.Ranks.Take(10), JsonRequestBehavior.AllowGet);
+            return Json(_rankContext.Ranks.OrderByDescending(x => x.Score).Take(10), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult SendRank(string name, int score)
+        {
+            _rankContext.Ranks.Add(new Models.RankModel() { Name = name, Score = score });
+            _rankContext.SaveChanges();
+            return Json(new { Status = "OK", Name = name ,Score = score }, JsonRequestBehavior.AllowGet);
         }
     }
 }
